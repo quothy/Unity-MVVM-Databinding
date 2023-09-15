59a75a0cf97569a664135becd60f9475e874d4da
quothy <heasley.katie@gmail.com>
2023-09-15 08:20:13 -0700

    Added support for binding to text input fields

--- com.kf.mvvm-data-binding/Runtime/Binders/TextBinder.cs
+++ com.kf.mvvm-data-binding/Runtime/Binders/TextBinder.cs
@@ -10,8 +10,6 @@ namespace MVVMDatabinding
     [Serializable]
     public class TextBinder : BaseBinder<string>
     {
-        //protected string name = "Text Binder";
-
         [SerializeField]
         private BindDirection bindDirection = BindDirection.OneWay;
 
@@ -19,13 +17,47 @@ namespace MVVMDatabinding
         [SerializeField]
         private TextMeshProUGUI text = null;
 
-        //public override string Name { get => name; }
+        [ConditionalVisibility(nameof(bindDirection), ConditionResultType.ShowIfNotEquals, (int)BindDirection.OneWay)]
+        [SerializeField]
+        private TMP_InputField textInput = null;
+
+        public override void Bind(GameObject bindingObject)
+        {
+            base.Bind(bindingObject);
+
+            if (bindDirection != BindDirection.OneWay && textInput != null)
+            {
+                textInput.onValueChanged.AddListener(OnTextEdited);
+            }
+        }
+
+        public override void Unbind()
+        {
+            base.Unbind();
+
+            if (bindDirection != BindDirection.OneWay && textInput != null)
+            {
+                textInput.onValueChanged.RemoveListener(OnTextEdited);
+            }
+        }
 
         protected override void OnDataUpdated(string dataValue)
         {
-            if (text != null)
+            if (text != null && bindDirection == BindDirection.OneWay)
             {
                 text.text = dataValue;
+            }
+            else if (textInput != null && bindDirection == BindDirection.TwoWay)
+            {
+                textInput.text = dataValue;
+            }
+        }
+
+        private void OnTextEdited(string textData)
+        {
+            if (TryGetDataSource(out IDataSource source))
+            {
+                source.TrySetItem<string>(itemId, textData);
             }
         }
     }
